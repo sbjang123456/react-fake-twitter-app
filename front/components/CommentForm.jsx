793bffb987abc027f08a00ac3845c4e8a5bc6d94
sbjang <sbjang123456@gmail.com>
2022-05-19 23:50:34 +0900

    add PostImages component

--- front/components/CommentForm.jsx
+++ front/components/CommentForm.jsx
@@ -7,12 +7,14 @@ import { useSelector } from 'react-redux';
 const CommentForm = ({ post }) => {
   const id = useSelector((state) => state.user.me?.id);
   const [commentText, onChangeCommentText] = useInput('');
-  const onSubmitComment = useCallback(() => {}, [commentText]);
+  const onSubmitComment = useCallback(() => {
+    console.log(post.id, commentText);
+  }, [commentText]);
 
   return (
     <Form onFinish={onSubmitComment}>
       <Form.Item style={{ position: 'relative', margin: 0 }}>
-        <Input.TextArea value={commentText} onChange={onChangeCommentText} />
+        <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4} />
         <Button
           style={{ position: 'absolute', right: 0, bottom: -40 }}
           type="primary"
